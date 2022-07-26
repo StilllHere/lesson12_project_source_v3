0925006d45178d9337a784b994c960ca85cb86a0
StilllHere <106540985+StilllHere@users.noreply.github.com>
2022-07-26 21:48:49 +0300

    add views with static_dir

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, send_from_directory
 from main.views import main_blueprint
 from loader.views import loader_blueprint
 
@@ -10,6 +10,10 @@ app.register_blueprint(main_blueprint)
 app.register_blueprint(loader_blueprint)
 
 logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler(filename="basic.log", encoding='utf-8', mode='a+')])
+
+@app.route("/uploads/<path:path>")
+def static_dir(path):
+    return send_from_directory("uploads", path)
 
 if __name__ == "__main__":
     app.run()
